2b080013944f9b61886ea578e34aa3a61d4d61e4
Jga5175 <155933398+Jga5175@users.noreply.github.com>
2024-01-15 00:08:33 -0500

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        String hello = "Hello";
+        String world = "world!";
+        System.out.printf("%s %s", hello, world);
+    }
+}
